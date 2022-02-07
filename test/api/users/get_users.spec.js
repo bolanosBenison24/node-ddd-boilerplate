44ad91a90600eacf077e94eb43ff755f7bbece54
Benison Bolanos <ben@offshorly.com>
2022-02-07 20:53:22 +0800

    test lint-staged command with error test

--- test/api/users/get_users.spec.js
+++ test/api/users/get_users.spec.js
@@ -52,7 +52,7 @@ describe('Routes: GET UsersEntity', () => {
     it('should return all users', (done) => {
       request.get(`${BASE_URI}/users`)
         .set('Authorization', `JWT ${token}`)
-        .expect(200)
+        .expect(404)
         .end((err, res) => {
           expect(res.body.data).to.have.length(2)
           done(err)
